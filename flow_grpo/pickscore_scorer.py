860d9a2cd2b0c991438909239b727e392439be43
RomanticQq <3236997288@qq.com>
2025-12-07 18:44:08 +0800

    update

--- flow_grpo/pickscore_scorer.py
+++ flow_grpo/pickscore_scorer.py
@@ -46,7 +46,7 @@ class PickScoreScorer(torch.nn.Module):
         scores = logit_scale * (text_embs @ image_embs.T)
         scores = scores.diag()
         # norm to 0-1
-        scores = scores/26
+        # scores = scores/26
         return scores
 
 # Usage example
