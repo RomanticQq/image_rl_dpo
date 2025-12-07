15d67e62d2df9a890b82c57ab788cc0e81bc647c
RomanticQq <3236997288@qq.com>
2025-12-07 17:10:38 +0800

    update

--- scripts/single_node/dpo.sh
+++ scripts/single_node/dpo.sh
@@ -1,1 +1,1 @@
-accelerate launch --config_file scripts/accelerate_configs/multi_gpu.yaml --num_processes=1 --main_process_port 29501 scripts/train_sd3_dpo.py --config config/dpo.py:pickscore_sd3
+nohup accelerate launch --config_file scripts/accelerate_configs/multi_gpu.yaml --num_processes=1 --main_process_port 29501 scripts/train_sd3_dpo.py --config config/dpo.py:pickscore_sd3 > output.log 2>&1 &
